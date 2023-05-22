9f05f95c52a767c4511e520fafa2e8e42ab31971
Ivanov Anton <112862863+IvanovAnton4682671@users.noreply.github.com>
2023-05-22 20:00:14 +0300

    Delete zadanie6 directory

--- zadanie6/delete.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $ID_Record = $_GET['id_record'];
-    $user = 'u52864';
-    $password = '3567354';
-    $database = new PDO('mysql:host=localhost;dbname=u52864', $user, $password, [PDO::ATTR_PERSISTENT => true, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-    $result = $database -> exec("DELETE FROM Information WHERE ID_Record = '$ID_Record'");
-    $result = $database -> exec("DELETE FROM Connection WHERE ID_Record = '$ID_Record'");
-?>
